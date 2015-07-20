8d5a1089e414df79d018a5ef31723c4c1dd7c712
Adam <adamdanferguson@gmail.com>
2015-07-20 10:01:52 +0100

    backup before messing with angular

--- app.py
+++ app.py
@@ -13,25 +13,104 @@ app = Flask(__name__)
 Triangle(app)
 
 PATH_TO_ANSIBLE = '/home/vagrant/ansible/ntdr-pas/playbooks/'    
+PATH_PYTHON_APP = '/home/vagrant/flaskStarter/'
 app.debug = True
+
+
+def command_parser(string_value,brandcode): 
+    string_value = re.split('\n\s*\n', string_value)  
+    
+    tempArray = []
+    for element in string_value:
+        if not element.strip() == '':
+            tempArray.append(element)
+
+    string_value = tempArray
+    tempArray = []
+        
+
+    for element in string_value:
+        element =  element.replace(brandcode+'_test | success >>','')
+        element =  element.replace(brandcode+'_live | success >>','')
+        tempArray.append(element)
+    
+    return tempArray
+
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=random.randint(1,1000))
+    return render_template('index.html')
 
-@app.route('/tom')
-def jsteaching():
-    return render_template('jsteaching.html', name=random.randint(1,1000))
+
+
+@app.route('/redesign')
+def redesign():
+    f = open('filetree.txt','r')
+    filetree_cache = f.read()
+    f.close()
+    return render_template('redesign.html', data = filetree_cache)
+
+
+
+
+
+#ansible -i inventory/cottage-servers zz -m ntdr_get_filetree.py -a path=/var/www
+@app.route('/getFiletree', methods=['GET'])
+def getFiletree():
+
+    server_codes = ['zz']
+    returnObj = {}
+    for code in server_codes:
+        print 'we are doing stuff honest'
+        os.chdir(PATH_TO_ANSIBLE)
+        filetree_command = ['ansible', '-i', 'inventory/cottage-servers', code, '-m', 'ntdr_get_filetree.py', '-a', 'path=/var/www']
+        filetree = subprocess.check_output(filetree_command)
+        filetree = command_parser(filetree,code)
+        
+        tasks = [
+                {
+                    'name':'Get file Tree from /var/www on the '+ code +' server group',
+                    'success':True,
+                    'errorMessage':None
+                }
+        ]
+
+        filetree_data = {
+                            "data":{
+                                    "test":json.loads(filetree[0])['stat']['files'],
+                                    "live":json.loads(filetree[1])['stat']['files']
+                            },
+                            "meta":{
+                                "tasks":tasks
+                            }
+        }
+        
+        returnObj[code] = filetree_data
+
+    returnObj = json.dumps(returnObj, separators =(',',':'))
+         
+    
+    f = open(PATH_PYTHON_APP+'filetree.txt','w')
+    f.write(returnObj)
+    f.close()
+
+
+
+
+    return returnObj
+
 
 
 #ansible -i inventory/cottage-servers zz -m ntdr_get_version.py -a path=/var/www -vvv
 @app.route('/getVersion', methods=['GET'])
 def getVersion():
     os.chdir(PATH_TO_ANSIBLE)
-    version_command =['ansible', '-i', 'inventory/cottage-servers',request.args['code'],  '-m', 'ntdr_get_version.py', '-a', 'path=/var/www/testing']
+    version_command =['ansible', '-i', 'inventory/cottage-servers',request.args['code'],  '-m', 'ntdr_get_version_improved.py', '-a', 'path='+request.args['path']]
     version_info = subprocess.check_output(version_command)
+
+    
     version_info = re.split('\n\s*\n', version_info)  
 
     tempArray = []
@@ -41,11 +120,29 @@ def getVersion():
         tempArray.append(element)
     
     version_info = tempArray
+   
+    #as this task is not running from a playbook the fact it is made of one task is hardcoded.
+    tasks = [
+             {
+                "name":"Get Versions available on servers",
+                "success":True,
+                "errorMessage":None
+             }
+    ]
 
-
-    versionData = '{"versionData":{ "test":' +version_info[0]+', "live":'+version_info[1]+ '}}'
-
-    return versionData
+    version_data = {
+                        'versionData':{
+                                'test':json.loads(version_info[0]),
+                                'live':json.loads(version_info[1])
+                        },
+                        'meta':{
+                            'tasks':tasks
+                        }
+                    }
+    
+    version_data = json.dumps(version_data, separators =(',',':'))
+    
+    return version_data
 
 
 
--- static/main.js
+++ static/main.js
@@ -1,35 +1,68 @@
-angular.module('mainApp').controller('mainController', function($scope, $http) {
+angular.module('mainApp').controller('mainController', function($scope,$rootScope) {
+
+
+	//$rootScope.cheese = 'cheddar';	
+	console.log($rootScope.cheese);
+	$scope.brandCodeSelected ='zz';
+	$scope.tick = "http://i.imgur.com/ATfTAaB.png" ;
+	$scope.cross = "http://i.imgur.com/PZvHtcx.png";
 	$scope.version = {"live":"","test":""};            
-	$scope.tasks ={'listVersions':false,
-		           'pushFix':false,
-                   'switchTestingLatest':false, 
-                   'releaseApproved':false,
-                   'rollBack':false,
-                   'localCopy':false,
-                   'updateCopy':false,
-                   'freshenRemote':false
+    $scope.activeTab = 'listVersions';	
+	$scope.controlList ={'listVersions':{
+										 'loading':false,
+										 'loaded':false, 
+										 'brandCode':null,
+										 'path':'/var/www',
+										 'results':null
+										 },
+
+ 						'pushFix':{'loading':false,
+								   'loaded':false, 
+						           },
+
+ 						'switchTestingLatest':{
+										 'loading':false,
+										 'loaded':false,
+						                 },
+
+ 						'releaseApproved':{
+										 'loading':false,
+										 'loaded':false,
+										 },
+
+ 						'rollBack':{
+						   			'loading':false,
+								    'loaded':false,	     
+								    },
+
+ 						'localCopy':{
+										 'loading':false,
+										 'loaded':false,
+										 
+										  },
+
+ 						'updateCopy':{
+										 'loading':false,
+										 'loaded':false,
+										 
+									 },
+
+ 						'freshenRemote':{
+										 'loading':false,
+										 'loaded':false,
+										 
+										},
 	};
 
-	$scope.loading ={'listVersions':false,
-		             'pushFix':false,
-                     'switchTestingLatest':false, 
-                     'releaseApproved':false,
-                     'rollBack':false,
-                     'localCopy':false,
-                     'updateCopy':false,
-                     'freshenRemote':false
-	};
+	$scope.server_data ={
+		'br':{},
+		'ch':{},
+		'ly':{},
+		'fbm':{},
+		'wl':{},
+		'zz':{}
+	}
 
-
-	$scope.loaded ={'listVersions':false,
-		            'pushFix':false,
-		            'switchTestingLatest':false, 
-		            'releaseApproved':false,
-	 	            'rollBack':false,
-		            'localCopy':false,
-		            'updateCopy':false,
-		            'freshenRemote':false
-	};
 
 	$scope.adminTab = 'active';
 	$scope.developerTab = '';
@@ -46,26 +79,36 @@ angular.module('mainApp').controller('mainController', function($scope, $http) {
 		if (tab == 'admin'){
 			$scope.adminTab ='active';
 			$scope.developerTab = '';
+			$scope.adminActions = true;
+			$scope.developerActions = false;
 		};
 	};
 
 	$scope.expandTask = function(task){
-		$scope.tasks[task] = !$scope.tasks[task]
+		$scope.activeTab = task;
+	};
+	
+	$scope.getFiletree = function(brand_code){
+		$http.get('/getFiletree?code='+brand_code).
+			success(function(data,status,headers,config){
+				$scope.server_data[brand_code].filetree = data.data;
+			}).
+			error(function(data,status,headers,config){
+				console.log('we messed up');
+			});
 	};
 
 
-	$scope.listVersions = function(brand_code){
-		console.log(brand_code);
-		$scope.loading.listVersions = true;
-		$http.get('/getVersion?code='+brand_code ).
+	$scope.listVersions = function(brand_code,path){
+		$scope.controlList.listVersions.loading =true;
+		$scope.controlList.listVersions.loaded = false;
+		$http.get('/getVersion?code='+brand_code+'&path='+path).
 			success(function(data,status,headers,config){
-				$scope.loading.listVersions = false;
-				console.log(data)
-				$scope.listOfVersions = data;
-			$scope.testVersion = data["versionData"]["test"]["stat"]["version"]
-				console.log($scope.testVersion = data["versionData"]["test"]["stat"]["version"])
+				$scope.controlList.listVersions.loading =false;
+				$scope.controlList.listVersions.loaded = true;
+				$scope.controlList.listVersions.results = data;
 			}).
-		error(function(data,status,headers,config){
-		});
+		    error(function(data,status,headers,config){
+		    });
 	};
 });
