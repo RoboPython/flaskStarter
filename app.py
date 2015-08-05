8b5e4f645f00cd5aedfb704d6990714f048ee85a
Hoolean <harry@hoolean.com>
2015-08-05 15:52:45 +0100

    Decoupled playbook yield conversion logic from web code

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ import re
 import json
 import random
 import os
-import old.oldapi
-import Queue 
-import threading 
 
 app = Flask(__name__)
 app.debug = True
@@ -24,32 +21,14 @@ app.debug = True
 Triangle(app)
 scss = Scss(app, static_dir='static/dep/styles', asset_dir='assets');
 
-config = open('pythonConfig.txt','r')
+config = open('config.json','r')
 config = json.loads(config.read())
 
 PATH_TO_ANSIBLE = config['path_to_ansible']
 PATH_PYTHON_APP = config['path_python_app']
 MYSQL_ROOT_PW = config['mysql_root_pw']
 PATH_TO_CACHE = config['path_python_app'] + 'cache/'
-
-def task_parser(string_value, brandcode): 
-    string_value = re.split('\n\s*\n', string_value)  
-    
-    tempArray = []
-    for element in string_value:
-        if not element.strip() == '':
-            tempArray.append(element)
-
-    string_value = tempArray
-    tempArray = []
-        
-
-    for element in string_value:
-        element =  element.replace(brandcode+'_test | success >>','')
-        element =  element.replace(brandcode+'_live | success >>','')
-        tempArray.append(element)
-    
-    return tempArray
+LIST_OF_SERVERS = config['list_of_servers']
 
 @app.route('/')
 def redesign():
@@ -58,12 +37,9 @@ def redesign():
     f.close()
     return render_template('index.html', data = filetree_cache)
 
-
-
 #ansible -i inventory/cottage-servers zz -m ntdr_get_filetree.py -a path=/var/www
 @app.route('/getFiletree', methods=['GET'])
 def getFiletree():
-
     server_codes = config['list_of_servers']
     returnObj = {}
     for code in server_codes:
@@ -72,31 +48,31 @@ def getFiletree():
         filetree_command = ['ansible', '-i', 'inventory/cottage-servers', code, '-m', 'ntdr_get_filetree.py', '-a', 'path=/var/www']
         filetree = subprocess.check_output(filetree_command)
         filetree = task_parser(filetree,code)
-        
+
         tasks = [
-                {
-                    'name':'Get file Tree from /var/www on the '+ code +' server group',
-                    'success':True,
-                    'errorMessage':None
-                }
+            {
+                'name': 'Get file Tree from /var/www on the '+ code +' server group',
+                'success': True,
+                'errorMessage': None
+            }
         ]
 
         filetree_data = {
-                            "data":{
-                                    "test":json.loads(filetree[0])['stat']['files'],
-                                    "live":json.loads(filetree[1])['stat']['files']
-                            },
-                            "meta":{
-                                "tasks":tasks
-                            },
-                            "code":code
+            "data":{
+                "test": json.loads(filetree[0])['stat']['files'],
+                "live": json.loads(filetree[1])['stat']['files']
+            },
+            "meta":{
+                "tasks": tasks
+            },
+            "code": code
         }
-        
+
         returnObj[code] = filetree_data
 
     returnObj = json.dumps(returnObj, separators =(',',':'))
-         
-    
+
+
     f = open(PATH_PYTHON_APP+'filetree.txt','w')
     f.write(returnObj)
     f.close()
@@ -113,43 +89,23 @@ ansible-playbook pull-full-copy.yml \
 
 @app.route('/localCopy',methods=['GET'])
 def localCopy():
-    callback_queue = Queue.Queue()
+    playbook_path = PATH_TO_ANSIBLE + '/pull-full-copy.yml'
+    inventory_path = PATH_TO_ANSIBLE + '/inventory'
 
-    def on_dict(returned_object):
-        callback_queue.put(returned_object)
-    
-
-    if request.args['serverType']:
+    if request.args['serverType'] != 'All':
         limit = request.args['code'] +'_'+request.args['serverType']
     else:
       return "nope can't accept that as a serverType"
 
+    extra_vars = { 'mysql_root_pw': MYSQL_ROOT_PW }
 
-
-    playbook_thread = threading.Thread(target=bridge.run_playbook, args=(PATH_TO_ANSIBLE + '/pull-full-copy.yml', PATH_TO_ANSIBLE + '/inventory',{'mysql_root_pw':MYSQL_ROOT_PW},limit, on_dict))
-    playbook_thread.start()
-    
     if request.headers.get('accept') == 'text/event-stream':
         def events():
-            for callback_json in iter(callback_queue.get,None):
-                print callback_json
-                yield "data: %s\n\n" %json.dumps(callback_json)
-                if callback_json['event'] == 'finished':
-                    break
-
-        
-        
+            for callback_json in bridge.run_playbook_yield_events(playbook_path, inventory_path, limit, extra_vars):
+                yield callback_json
         return Response(events(), content_type='text/event-stream')
     else:
         return 'Just in case the front end messes up its header LOL'
 
-    
-
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000)
-    
-
-
-
-
-
--- tachyon/bridge.py
+++ tachyon/bridge.py
@@ -1,3 +1,7 @@
+import json
+import Queue
+import threading
+
 from ansible.playbook import PlayBook
 from ansible.inventory import Inventory # TODO - check if needed
 from ansible import callbacks
@@ -5,8 +9,7 @@ from ansible import utils # TODO - check if needed
 
 from tachyon.emitter_callbacks import EmitterCallbacks
 
-
-def run_playbook(playbook_path, inventory_path, extra_vars,subset, event_callback):
+def run_playbook_call_callback(playbook_path, inventory_path, extra_vars, subset, event_callback):
     callbacks_object = EmitterCallbacks(event_callback)
     stats = callbacks.AggregateStats()
     pb = PlayBook(
@@ -19,6 +22,19 @@ def run_playbook(playbook_path, inventory_path, extra_vars,subset, event_callbac
         subset           =   subset
     )
     results = pb.run()
-    
+
     callbacks_object.on_complete()
-    
+
+def run_playbook_yield_events(playbook_path, inventory_path, subset, extra_vars):
+    callback_queue = Queue.Queue()
+
+    def on_dict(returned_object):
+        callback_queue.put(returned_object)
+
+    playbook_thread = threading.Thread(target=run_playbook_call_callback, args=(playbook_path, inventory_path, extra_vars, subset, on_dict))
+    playbook_thread.start()
+
+    for callback_json in iter(callback_queue.get,None):
+        yield "data: %s\n\n" %json.dumps(callback_json)
+        if callback_json['event'] == 'finished':
+            break
