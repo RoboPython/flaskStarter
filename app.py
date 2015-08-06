4993501bba386794d9ed8fc8bd3f7685b27b058e
Adam <adamdanferguson@gmail.com>
2015-08-06 13:57:07 +0100

    working commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -30,6 +31,8 @@ MYSQL_ROOT_PW = config['mysql_root_pw']
 PATH_TO_CACHE = config['path_python_app'] + 'cache/'
 LIST_OF_SERVERS = config['list_of_servers']
 
+
+
 playbooks = None
 with open('config/playbooks.json', 'r') as playbooks_file:
     try:
@@ -37,6 +40,8 @@ with open('config/playbooks.json', 'r') as playbooks_file:
         playbooks = json.loads(playbooks_raw)
     except Exception as e:
         print('Could not load playbooks.json:', str(e))
+
+
 
 @app.route('/')
 def redesign():
@@ -52,18 +57,80 @@ def redesign():
 
 @app.route('/playbooks.json')
 def get_playbooks():
-    playbooks_raw = json.dumps(playbooks)) 
+    playbooks_raw = json.dumps(playbooks)
     return Response(response=playbooks_raw, status=200, mimetype="application/json")
+
+
 
 @app.route('/run_playbook', methods=['POST'])
 def call_run_playbook():
     shortname = None
+    print request.form
+    print request.args
     if "shortname" in request.form:
         shortname = request.form["shortname"]
     else:
-        return flask.jsonify(error="'shortname' parameter was missing")
+        return flask.jsonify(error="'shortname' parameter was missing"),400
+    playbook = None
+    for playbookCandidate in playbooks["playbooks"]:
+        if playbookCandidate['shortname'] == shortname:
+            playbook = playbookCandidate
+    if playbook == None:
+        return flask.jsonify(error="'shortname' value '" + shortname + "' does not exist")
+    extra_vars = {}
+    for field in playbook['fields']:
+        if not field['name'] in request.form:
+            if field['required']:
+                return flask.jsonify(error="Field '" + field['name'] + "' is required")
+            else:
+                # TODO: implement default values
+                continue
+        else:
+            value = request.form[field['name']]
+            returnTypeCaster = None
+            if field['returnType'] == 'string':
+                returnTypeCaster = str
+            elif field['returnType'] == 'boolean':
+                returnTypeCaster = bool
+            elif field['returnType'] == 'integer':
+                returnTypeCaster = int
+            try:
+                castValue = returnTypeCaster(value)
+            except Exception as e:
+                return flask.jsonify(error="Field '" + field['name'] + "' could not be cast: " + str(e))
+            if castValue != False:
+                extra_vars[field['name']] = castValue
+    
+    for configObj in playbook['configNodes']:
+        extra_vars[configObj["argName"]] = config[configObj['node']]
 
+    print(json.dumps(extra_vars))
 
+    playbook_path = PATH_TO_ANSIBLE + '/' + playbook['yaml']
+    inventory_path = PATH_TO_ANSIBLE + '/inventory'
+
+    if "code" in request.form:
+        code = request.form["code"]
+    else:
+        return flask.jsonify(error="'code' parameter was missing")
+
+    if "serverType" in request.form:
+        server_type = request.form["serverType"]
+    else:
+        return flask.jsonify(error=" 'serverType' parameter was missing")
+
+    if server_type  != 'All':
+        limit = code +'_'+ server_type
+    else:
+        return flask.jsonify(error=" You cannot do operations on multiple servers at once")
+
+    if request.headers.get('accept') == 'text/event-stream':
+        def events():
+            for callback_json in bridge.run_playbook_yield_events(playbook_path, inventory_path, limit, extra_vars):
+                yield callback_json
+        return Response(events(), content_type='text/event-stream')
+    else:
+        return '\n Error: request header is not "text/event-stream" \n'
 
 #ansible -i inventory/cottage-servers zz -m ntdr_get_filetree.py -a path=/var/www
 @app.route('/getFiletree', methods=['GET'])
